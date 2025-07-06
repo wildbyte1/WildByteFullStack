f3764d24f50493eface41ad93eca05718d17bae4
Angelica Victoria <angelica@avictoria.dev>
2025-07-05 22:48:32 -0700

    added client assets

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32'>
+        <img src={assets} alt='' />
+      
+    </div>
+  )
+}
+
+export default Navbar
