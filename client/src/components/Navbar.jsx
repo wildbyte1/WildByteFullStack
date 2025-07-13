3c48542e7d1b50e9eabc1586bbff7a6b5413ae53
Angelica Victoria <angelica@avictoria.dev>
2025-07-12 18:34:18 -0700

    updated assets.js and Navbar.js

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { assets } from '../assets/assets.js'
 
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32'>
-        <img src={assets} alt='' />
+        <img src={assets.Original_Logo} alt='WildByte Logo' className='w-32 sm:w-44'/>
+        <button>Login 
+          <img src={assets.arrow} className='w-3' alt='arrow' />
+        </button>
       
     </div>
   )
