4a24b25cae6243e3db45d71586f1d05ff9e8bd9f
Angelica Victoria <angelica@avictoria.dev>
2025-07-14 21:30:28 -0700

    updated Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import { assets } from '../assets/assets.js'
 
 const Header = () => {
   return (
     <div className='mx-8 sm:mx-16 xl:mx-24 relative'>
-      <div>
-        <div>
-            <p>New: AI feature integrated</p>
+      <div className='text-center mt-20 mb-8'>
+
+        <div className='inline-flex items-center justify-center gap-4 px-6 py-1.5 mb-4 border border-primary/40 bg-primary/10 rounded-full text-sm text-text'>
+          <p>New: AI feature integrated </p>
+          <img src={assets.star_solid} className='w-2.5' alt='' />
         </div>
       </div>
-      <img src={assets.gradient} alt='' className='absolute -top-50 -z-1 opacity-50' />
+      <img
+        src={assets.gradient}
+        alt=''
+        className='absolute -top-20 -z-1 opacity-15'
+      />
     </div>
-  )
+  );
 }
 
 export default Header
