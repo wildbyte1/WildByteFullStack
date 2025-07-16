43107c0af9e5b06bad96d30968933f8e2750172c
Angelica Victoria <angelica@avictoria.dev>
2025-07-15 20:20:37 -0700

    added motion npm and updated BlogList.jsx

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { blogCategories } from '../assets/assets'
+import { motion } from 'motion/react';
 
 const BlogList = () => {
 
@@ -17,7 +18,10 @@ const BlogList = () => {
               }`}>
               {item}
               {menu === item && (
-                <div className='absolute left-0 right-0 top-0 h-7 -z-1 bg-secondary rounded-full'></div>
+                <motion.div 
+               layoutId='underline'
+               transition={{type: 'spring', stiffness: 500, damping: 30}}
+               className='absolute left-0 right-0 top-0 h-7 -z-1 bg-secondary rounded-full'></motion.div>
               )}
               
             </button>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { assets } from '../assets/assets.js'
 import { useNavigate } from 'react-router-dom'
 
+
 const Navbar = () => {
 
 const navigate = useNavigate();
