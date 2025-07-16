9021ccd24eb7d0e7ea0f8f8795a6a2c717fd62c6
Angelica Victoria <angelica@avictoria.dev>
2025-07-15 21:13:46 -0700

    updated blog paths in App.jsx

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -7,15 +7,23 @@ const BlogCard = ({blog}) => {
     const navigate = useNavigate()
 
   return (
-    <div onClick={()=> navigate(`/blog/${_id}`)} className='w-full rounded-lg overflow-hidden shadow hover:scale-102 hover:shadow-primary/25 duration-300 cursor-pointer'> 
-      <img src={image} alt="" className='aspect-video' />
-      <span className='ml-5 mt-4 px-3 py-1 inline-block bg-secondary/70 rounded-full text-text text-xs'>{category}</span>
+    <div
+      onClick={() => navigate(`/blog/${_id}`)}
+      className='w-full rounded-lg overflow-hidden shadow hover:scale-102 hover:shadow-primary/25 duration-300 cursor-pointer'
+    >
+      <img src={image} alt='' className='aspect-video' />
+      <span className='ml-5 mt-4 px-3 py-1 inline-block bg-secondary/70 rounded-full text-text text-xs'>
+        {category}
+      </span>
       <div className='p-5'>
         <h5 className='mb-2 font-medium text-gray-900'>{title}</h5>
-        <p className='mb-3 text-xs text-gray-600'>{description.slice(0,80)}</p>
+        <p
+          className='mb-3 text-xs text-gray-600'
+          dangerouslySetInnerHTML={{ __html: description.slice(0, 80)}}
+        ></p>
       </div>
     </div>
-  )
+  );
 }
 
 export default BlogCard
