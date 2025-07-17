d78d59cb511f6039ba84b90e27ff18e8e07e0fb6
Angelica Victoria <angelica@avictoria.dev>
2025-07-16 19:17:47 -0700

    created Newsletter.jsx

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -9,7 +9,7 @@ const BlogList = () => {
 
   return (
     <div>
-      <div className='flex justify-center gap-4 sm:gap-8 my-10 relative'>
+      <div className='flex flex-wrap justify-center gap-4 sm:gap-8 my-10 relative'>
         {blogCategories.map((item) => (
           <div key={item} className='relative'>
             <button
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import BlogList from '../components/BlogList'
+import Newsletter from '../components/Newsletter'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar/>
       <Header/>
       <BlogList/>
+      <Newsletter/>
     </>
   )
 }
