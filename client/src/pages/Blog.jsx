90ec94a5e6643f585a43d19bd1bf7f81ecda7be6
Angelica Victoria <angelica@avictoria.dev>
2025-07-17 15:30:26 -0700

    Created Blog.jsx

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useParams } from 'react-router-dom';
+import { blog_data } from '../assets/assets';
+import Navbar from '../components/Navbar';
 
 const Blog = () => {
-  return (
+
+  const {id} = useParams()
+
+  const [data, setData] = useState(null)
+
+  const fetchBlogData = async () => {
+    blog_data.find(item => item.id === id)
+    setData(data) 
+
+  }
+
+  useEffect(() => {
+    fetchBlogData()
+  }, [])
+
+  return data ? (
     <div>
-      <h1>Blog</h1>
+
+      <Navbar/>
+      <div></div>
+
+      <div></div>
     </div>
-  )
+  ) : <div>Loading... </div>
 }
 
 export default Blog
