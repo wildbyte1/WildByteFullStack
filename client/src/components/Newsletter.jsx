970c1c95139736e9a7de5ff2a9d489a8a0ae4b3b
Angelica Victoria <angelica@avictoria.dev>
2025-07-16 20:52:17 -0700

    updated Newsletter.jsx

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Newsletter = () => {
   return (
     <div className='flex flex-col items-center justify-center text-center space-y-2 my-32'>
-      <h1 className='md:text-4xl text-2xl font-semibold'>Never Miss a Blog!</h1>
-      <p className='md:text-lg text-text pb-8'>Subscribe to get the latest stories from the wild and, exclusive content. </p>
+      <h1 className='md:text-4xl text-2xl font-semibold text-accent'>Never Miss a Blog!</h1>
+      <p className='md:text-lg text-text pb-8'>Subscribe to get the latest stories from the wild and exclusive content. </p>
       <form className='flex items-center justify-between max-w-2xl w-full md:h-13 h-12'>
         <input className='border border-gray-300 rounded-md h-full border-r-0 outline-none w-full rounded-r-none px-3 text-gray-500' type='text' placeholder='Enter your email' required/>
         <button type='submit' className='md:px-12 px-8 h-full text-white bg-text hover:bg-secondary transition-all cursor-pointer rounded-md'>Subscribe</button>
