411f9474eed99a00967bcf4bae256c82a04f208f
Angelica Victoria <angelica@avictoria.dev>
2025-07-17 20:33:47 -0700

    updated index.css and Blog.jsx

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-import { assets, blog_data } from '../assets/assets.js';
+import { assets, blog_data, comments_data } from '../assets/assets.js';
 import Navbar from '../components/Navbar';
 import Moment from 'moment';
 
@@ -9,6 +9,7 @@ const Blog = () => {
   const {id} = useParams()
 
   const [data, setData] = useState(null)
+  const [comments, setComments] = useState([]);
 
   const fetchBlogData = async () => {
     const data = blog_data.find(item => item._id === id)
@@ -16,8 +17,13 @@ const Blog = () => {
 
   }
 
+  const fetchComments = async () => {
+    setComments(comments_data)
+  }
+
   useEffect(()=>{
     fetchBlogData()
+    fetchComments()
   },[])
 
   return data ? (
@@ -38,18 +44,36 @@ const Blog = () => {
           {data.title}
         </h1>
         <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
-        <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-accent bg-primary font-medium text-text'>
+        <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-accent bg-background font-medium text-text'>
           Vesey Victoria
         </p>
       </div>
 
-      <div className='mx-5 max-w-3xl md:mx-auto my-10 mt-6'>
+      <div className='mx-5 max-w-3xl md:mx-auto my-10 mt-2'>
         <img src={data.image} alt='' className='rounded-3xl mb-5' />
 
         <div
           className='rich-text max-w-3xl mx-auto'
           dangerouslySetInnerHTML={{ __html: data.description }}
         ></div>
+
+        {/* comments section */}
+    <div className='mt-14 mb-10 max-w-3xl mx-auto'>
+      <p className='font-semibold mb-4'>Comments ({comments.length})</p>
+      <div className='flex flex-col gap-4'>
+        {comments.map((item, index)=>(
+          <div key={index} className='relative bg-secondary-background border border-secondary max-w-xl p-4 rounded text-text'>
+            <div className='flex items-center gap-2 mb-2 '>
+              <img src={assets.user_icon} alt='user icon' className='w-6' />
+              <p className='font-medium'>{item.name}</p>
+            </div>
+            <p className='text-sm max-w-md ml-8'>{item.content}</p>
+            <div className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>{Moment(item.createdAt).fromNow()}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+
       </div>
     </div>
   ) : (
