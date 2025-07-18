93e64c106996c110a218996c8fe9ae0d3c1b8cb0
Angelica Victoria <angelica@avictoria.dev>
2025-07-17 21:15:25 -0700

    updated Blog.jsx

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -11,6 +11,9 @@ const Blog = () => {
   const [data, setData] = useState(null)
   const [comments, setComments] = useState([]);
 
+  const [name, setName] = useState('');
+  const [content, setContent] = useState('');
+
   const fetchBlogData = async () => {
     const data = blog_data.find(item => item._id === id)
     setData(data) 
@@ -19,6 +22,11 @@ const Blog = () => {
 
   const fetchComments = async () => {
     setComments(comments_data)
+  }
+
+  const addComment = async (e) => {
+    e.preventDefault();
+    
   }
 
   useEffect(()=>{
@@ -58,22 +66,57 @@ const Blog = () => {
         ></div>
 
         {/* comments section */}
-    <div className='mt-14 mb-10 max-w-3xl mx-auto'>
-      <p className='font-semibold mb-4'>Comments ({comments.length})</p>
-      <div className='flex flex-col gap-4'>
-        {comments.map((item, index)=>(
-          <div key={index} className='relative bg-secondary-background border border-secondary max-w-xl p-4 rounded text-text'>
-            <div className='flex items-center gap-2 mb-2 '>
-              <img src={assets.user_icon} alt='user icon' className='w-6' />
-              <p className='font-medium'>{item.name}</p>
-            </div>
-            <p className='text-sm max-w-md ml-8'>{item.content}</p>
-            <div className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>{Moment(item.createdAt).fromNow()}</div>
+        <div className='mt-14 mb-10 max-w-3xl mx-auto'>
+          <p className='font-semibold mb-4'>Comments ({comments.length})</p>
+          <div className='flex flex-col gap-4'>
+            {comments.map((item, index) => (
+              <div
+                key={index}
+                className='relative bg-secondary-background border border-secondary max-w-xl p-4 rounded text-text'
+              >
+                <div className='flex items-center gap-2 mb-2 '>
+                  <img src={assets.user_icon} alt='user icon' className='w-6' />
+                  <p className='font-medium'>{item.name}</p>
+                </div>
+                <p className='text-sm max-w-md ml-8'>{item.content}</p>
+                <div className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>
+                  {Moment(item.createdAt).fromNow()}
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
 
+        {/* Add Comment Section */}
+        <div className='max-w-3xl mx-auto'>
+          <p className='font-semibold mb-4'>Add your comment</p>
+          <form
+            onSubmit={addComment}
+            className='flex flex-col items-start gap-4 max-w-lg'
+          >
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              type='text'
+              placeholder='Name'
+              required
+              className='w-full p-2 border border-gray-300 rounded outline-none'
+            />
+            <textarea
+              onChange={(e) => setContent(e.target.value)}
+              value={content}
+              placeholder='Comment'
+              className='w-full p-2 border border-gray-300 rounded outline-none h-48'
+              required
+            ></textarea>
+            <button
+              type='submit'
+              className='bg-accent text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer'
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   ) : (
