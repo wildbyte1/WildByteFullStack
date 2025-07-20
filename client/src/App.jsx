e81acbbf0fc716aa49c260fc71f67111cb16ee13
Angelica Victoria <angelica@avictoria.dev>
2025-07-19 20:05:44 -0700

    added admin components to pages folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import AddBlog from './pages/admin/AddBlog'
+import ListBlog from './pages/admin/ListBlog'
+import Comments from './pages/admin/Comments'
 
 const App = () => {
   return (
@@ -9,6 +14,12 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/blog/:id' element={<Blog/>} />
+        <Route path='/admin' element={<Layout/>}>
+        <Route index element={<Dashboard/>} />
+        <Route path='addBlog' element={<AddBlog/>} />
+        <Route path='listBlog' element={<ListBlog/>} />
+        <Route path='comments' element={<Comments/>} />
+        </Route>
       </Routes>
     </div>
   )
--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import { assets } from '../../assets/assets.js'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 const Layout = () => {
+
+    const navigate = useNavigate()
+
+    const logout = () => {
+        navigate('/')
+    }
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className='flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-secondary'>
+        <img
+          src={assets.Transparent_Logo}
+          alt='Wild Byte Logo'
+          className='h-20 sm:h-23 cursor-pointer object-contain '
+          onClick={() => navigate('/')}
+        />
+        <button
+          onClick={logout}
+          className='text-sm px-8 py-2 bg-accent text-white rounded-full cursor-pointer'
+        >
+          Logout
+        </button>
+      </div>
+      <div className='flex h-[calc(100vh-70px)]'>
+         <div>sidebar</div>
+         <Outlet />
+      </div>
+    </>
+  );
 }
 
 export default Layout
