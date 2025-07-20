d614dc8b899e6ac0cb078719b920a9cf078ae0ac
Angelica Victoria <angelica@avictoria.dev>
2025-07-19 20:34:05 -0700

    updated Login.jsx

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className='flex items-center justify-center h-screen'>
+      <div className='w-full max-w-sm p-6 max-md:m-6 border border-primary shadow-xl shadow-accent rounded-lg'>
+        <div className='flex flex-col items-center justify-center'>
+            <div className='w-full py-6 text-center'>
+               <h1 className='text-3xl font-bold'><span className='text-text'>Admin</span> Login</h1> 
+               <p className='font-light'>Enter your credentials to access the admin panel</p>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
