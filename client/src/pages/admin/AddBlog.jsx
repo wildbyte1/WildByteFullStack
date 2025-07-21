21b13358766acf18720855fa98743ff9af85ba16
Angelica Victoria <angelica@avictoria.dev>
2025-07-21 15:06:40 -0700

    updated AddBllog.jsx

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -15,18 +15,49 @@ const AddBlog = () => {
 
 
   return (
-    <form onSubmit={onSubmitHandler} className='flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll'>
+    <form
+      onSubmit={onSubmitHandler}
+      className='flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll'
+    >
       <div className='bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded '>
+        <p>Upload thumbnail</p>
+        <label htmlFor='image'>
+          <img
+            src={!image ? assets.upload_area : URL.createObjectURL(image)}
+            alt='Upload Area'
+            className='mt-2 h-16 rounded cursor-pointer'
+          />
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type='file'
+            id='image'
+            hidden
+            required
+          />
+        </label>
 
-      <p>Upload thumbnail</p>
-      <label htmlFor='image'>
-        <img src={!image ? assets.upload_area : URL.createObjectURL(image)} alt='Upload Area' className='mt-2 h-16 rounded cursor-pointer'/>
-        <input onChange={(e)=> setImage(e.target.files[0])} type='file' id='image' hidden required/>
-      </label>
+        <p className='mt-4'>Blog title</p>
+        <input
+          type='text'
+          placeholder='Type here'
+          required
+          className='w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded'
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+
+        <p className='mt-4'>Subtitle</p>
+        <input
+          type='text'
+          placeholder='Type here'
+          required
+          className='w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded'
+          onChange={(e) => setSubTitle(e.target.value)}
+          value={subTitle}
+        />
       </div>
     </form>
-         
-  )
+  );
 }
 
 export default AddBlog
