2c4403f8a3db556de0e4df62b6b2cd3cec1b6c9f
Angelica Victoria <angelica@avictoria.dev>
2025-07-21 16:59:15 -0700

    updated AddBlog.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import AddBlog from './pages/admin/AddBlog'
 import ListBlog from './pages/admin/ListBlog'
 import Comments from './pages/admin/Comments'
 import Login from './components/admin/Login'
+import 'quill/dist/quill.snow.css'
 
 const App = () => {
   return (
--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { assets } from '../../assets/assets'
+import Quill from 'quill';
 
 const AddBlog = () => {
+
+  const editorRef = useRef(null)
+  const quillRef = useRef(null)
 
   const [image, setImage] = useState(false);
   const [title, setTitle] = useState('');
@@ -9,9 +13,20 @@ const AddBlog = () => {
   const [category, setCategory] = useState('Startup');
   const [isPublished, setIsPublished] = useState(false);
 
+  const generateContent = async ()=>{
+
+  }
+
   const onSubmitHandler = async (e) => {
     e.preventDefault();
   }
+
+  useEffect(()=>{
+    //Initiate Quill only once
+    if(!quillRef.current && editorRef.current){
+      quillRef.current = new Quill(editorRef.current, {theme: 'snow'})
+    }
+  },[])
 
 
   return (
@@ -55,6 +70,14 @@ const AddBlog = () => {
           onChange={(e) => setSubTitle(e.target.value)}
           value={subTitle}
         />
+
+      <p className='mt-4'>Blog Description</p>
+      <div className='max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative'>
+        <div ref={editorRef}></div>
+        <button type='button' onClick={generateContent} className='absolute bottom-1 right-2 ml-2 text-xs text-white bg-accent/70 px-4 py-1.5 rounded hover:underline cursor-pointer'>Generate with AI</button>
+
+      </div>
+
       </div>
     </form>
   );
