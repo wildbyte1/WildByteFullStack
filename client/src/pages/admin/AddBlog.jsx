eb84d2d6d7f6747c9b3b0cc7aaab12324a5503a0
Angelica Victoria <angelica@avictoria.dev>
2025-07-21 14:59:09 -0700

    updated AddBlog.jsx

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../../assets/assets'
 
 const AddBlog = () => {
+
+  const [image, setImage] = useState(false);
+  const [title, setTitle] = useState('');
+  const [subTitle, setSubTitle] = useState('');
+  const [category, setCategory] = useState('Startup');
+  const [isPublished, setIsPublished] = useState(false);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  }
+
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={onSubmitHandler} className='flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll'>
+      <div className='bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded '>
+
+      <p>Upload thumbnail</p>
+      <label htmlFor='image'>
+        <img src={!image ? assets.upload_area : URL.createObjectURL(image)} alt='Upload Area' className='mt-2 h-16 rounded cursor-pointer'/>
+        <input onChange={(e)=> setImage(e.target.files[0])} type='file' id='image' hidden required/>
+      </label>
+      </div>
+    </form>
+         
   )
 }
 
