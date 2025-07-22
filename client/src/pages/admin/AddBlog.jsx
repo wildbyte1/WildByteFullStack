ae5bfd0c582b879a156245fad537bd8ad43c0ee3
Angelica Victoria <angelica@avictoria.dev>
2025-07-21 19:57:49 -0700

    updated AddBlog.jsx

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { assets } from '../../assets/assets'
+import { assets, blogCategories } from '../../assets/assets'
 import Quill from 'quill';
 
 const AddBlog = () => {
@@ -75,8 +75,20 @@ const AddBlog = () => {
       <div className='max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative'>
         <div ref={editorRef}></div>
         <button type='button' onClick={generateContent} className='absolute bottom-1 right-2 ml-2 text-xs text-white bg-accent/70 px-4 py-1.5 rounded hover:underline cursor-pointer'>Generate with AI</button>
-
       </div>
+      <p className='mt-4'>Blog category</p>
+      <select onChange={e => setCategory(e.target.value)} name='category' className='mt-2 px-3 py-2 border text-gray-500 border-gray-300 outline-none rounded'>
+        <option value=''>Select category</option>
+        {blogCategories.map((item, index)=>{
+          return <option key={index} value={item}>{item}</option>
+        })}
+      </select>
+
+      <div className='flex gap-2 mt-4'>
+        <p>Publish Now</p>
+        <input type='checkbox' checked={isPublished} className='scale-125 cursor-pointer' onChange={e => setIsPublished(e.target.checked)}></input>
+      </div>
+      <button type='submit'className='mt-8 w-40 h-10 bg-accent text-white rounded cursor-pointer text-sm'>Add Blog</button>
 
       </div>
     </form>
