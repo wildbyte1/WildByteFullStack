9d64419f1b98a7961e62f92d49c9a02ee33740f4
Angelica Victoria <angelica@avictoria.dev>
2025-07-22 19:58:36 -0700

    Created adminController.js

---
+++ server/controllers/adminController.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken'
+
+export const adminLogin = async (req, res)=>{
+    try {
+        const {email, password} = req.body;
+
+        if(email !== process.env.ADMIN_EMAIL || password !== process.env.ADMIN_PASSWORD){
+            return res.json({success: false, message: "Invalid Credentials"})
+        }
+
+        const token = jwt.sign({ email }, process.env.JWT_SECRET)
+        res.json({success: true, token})
+    } catch (error) {
+        res.json({ success: false, message: error.message});
+
+    }
+}
