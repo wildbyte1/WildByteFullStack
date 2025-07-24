f5da24a808f29f555019d870c7ae89cf5926431c
Angelica Victoria <angelica@avictoria.dev>
2025-07-23 20:31:20 -0700

    updated blogController.js and Blog.js

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,4 +1,6 @@
-
+import fs from 'fs'
+import imagekit from '../configs/imageKit.js';
+import Blog from '../models/Blog.js'
 
 export const addBlog = async (req, res)=>{
     try {
@@ -10,6 +12,34 @@ export const addBlog = async (req, res)=>{
           if(!title || !description || !category || !imageFile){
             return res.json({success: false, message: "Missing required fields"})
           }
+
+          const fileBuffer = fs.readFileSync(imageFile.path)
+
+          // Upload Image to ImageKit
+          const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: "/blogs"
+          })
+
+          // optimization through imagekit URL transformation
+          const optimizedImageURL = imagekit.url({
+            path: response.filePath,
+            transformation: [
+              {quality: 'auto'}, // Auto compression
+              {format: 'webp'}, // Convert to modern format
+              {width: '1280'}  // Width resizing
+            ]
+          });
+
+          const image = optimizedImageURL;
+
+          await Blog.create({title, subTitle, description, category, image, isPublished})
+
+          res.json({success: true, message: "Blog added successfully 🎊"})
+
+
     }  catch (error) {
+      res.json({ success: false, message: error.message });
 }
 }
