eae8e943875f69ace6c72627815d0e0fad274541
Angelica Victoria <angelica@avictoria.dev>
2025-07-24 21:21:59 -0700

    updated blogController.js
    Created API for blog list, details, delete and update.

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -43,3 +43,47 @@ export const addBlog = async (req, res)=>{
       res.json({ success: false, message: error.message });
 }
 }
+
+export const getAllBlogs = async (req, res)=>{
+  try {
+    const blogs =await Blog.find({isPublished: true})
+    res.json({ success: true, blogs})
+  } catch (error){
+      res.json({ success: false, message: error.message })
+  }
+}
+
+export const getBlogById = async (req, res) =>{
+  try {
+    const { blogId } = req.parse;
+    const blog = await Blog.findById(blogId)
+    if(!blog){
+        return res.json({ success: false, message: "Blog not found 😕"});
+    }
+    res.json({ success: true, blog })
+  } catch (error){
+      res.json({ success: false, message: error.message })
+  }
+}
+
+export const deletegetBlogById = async (req, res) => {
+  try {
+    const { id } = req.body;
+    await Blog.findByIdAndDelete(id);
+    res.json({ success: true, message: "Blog deleted successfully" })
+  } catch (error) {
+    res.json({ success: false, message: error.message })
+  }
+}
+
+export const togglePublish = async (req, res) =>{
+  try {
+    const { id } = req.body;
+    const blog = await Blog.findById(id);
+    blog.isPublished = !blog.isPublished;
+    await blog.save();
+    res.json({ success: true, message: 'Blog status updated' });
+  } catch (error) {
+    res.json({success: false, message: error.message})
+  }
+}
