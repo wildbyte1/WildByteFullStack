4e717fee2c22a3fd2e9c180d375f10ce3c9c6d6b
Angelica Victoria <angelica@avictoria.dev>
2025-07-24 18:53:43 -0700

    updated blogRoute.js, server.js, and created auth.js

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,6 +1,10 @@
 import express from "express";
 import { addBlog } from "../controllers/blogController.js";
+import upload from "../middleware/multer.js";
+import auth from "../middleware/auth.js";
 
 const blogRouter = express.Router();
 
-blogRouter.post("/add", addBlog)
+blogRouter.post("/add", upload.single('image'), auth, addBlog)
+
+export default blogRouter;
