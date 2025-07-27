c04c4b142d837b3cd248a5cc40788f719ee8324b
Angelica Victoria <angelica@avictoria.dev>
2025-07-27 14:31:20 -0700

    updated blogController.js

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import imagekit from '../configs/imageKit.js';
 import Blog from '../models/Blog.js';
+import Comment from '../models/Comment.js';
 
 export const addBlog = async (req, res) => {
   try {
@@ -97,8 +98,20 @@ export const togglePublish = async (req, res) => {
 
 export const addComment = async (req, res) =>{
   try {
-    
+      const {blog, name, content } = req.body;
+      await Comment.create({blog, name, content});
+      res.json({ success: true, message: 'Comment added for review' });
   } catch (error) {
     res.json({success: false, message: error.message})
+  }
+}
+
+export const getBlogComments = async (req, res) =>{
+  try {
+    const { blogId } = req.body;
+    const comments = await Comment.find({blog: blogId, isApproved: true}).sort({createdAt: -1});
+    res.json({success: true, comments})
+  } catch (error) {
+    res.json({ success: false, message: error.message });
   }
 }
