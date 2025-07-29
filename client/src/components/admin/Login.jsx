7361ee9c797e09950cca87db07b5d1cddd25aab1
Angelica Victoria <angelica@avictoria.dev>
2025-07-28 20:40:21 -0700

    connect backend APIs with Frontend blog app

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import { useState } from 'react'
+import { useAppContext } from '../../context/AppContext'
+import toast from 'react-hot-toast';
 
 const Login = () => {
 
-    const [email, setEmail] = useState('')
+  const {axios, setToken} = useAppContext();
+
+  const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     const handleSubmit = async (e)=>{
     e.preventDefault()
+    try {
+      const {data} = await axios.post('/api/admin/login', {email, password})
+
+      if(data.success){
+        setToken(data.token)
+        localStorage.setItem('token', data.token)
+        axios.defaults.headers.common['Authorization'] = data.token;
+      }
+      else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
     }
 
   return (
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -26,6 +26,11 @@ export const AppProvider = ({ children })=>{
 
     useEffect(()=>{
         fetchBlogs();
+        const token = localStorage.getItem('token')
+        if(token){
+            setToken(token);
+           axios.defaults.headers.common['Authorization'] = `${token}`;
+        }
     },[])
     
     const value = {
