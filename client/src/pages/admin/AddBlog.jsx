927bca6cc0097452f37f13ce14affba64f77a3a1
Angelica Victoria <angelica@avictoria.dev>
2025-07-29 21:30:15 -0700

    updated AddBlog.jsx
    connected backend apis with frontend blog app

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { assets, blogCategories } from '../../assets/assets'
 import Quill from 'quill';
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 const AddBlog = () => {
+
+  const {axios} = useAppContext()
+  const [isAdding, setIsAdding] = useState(false)
 
   const editorRef = useRef(null)
   const quillRef = useRef(null)
@@ -10,7 +15,7 @@ const AddBlog = () => {
   const [image, setImage] = useState(false);
   const [title, setTitle] = useState('');
   const [subTitle, setSubTitle] = useState('');
-  const [category, setCategory] = useState('Startup');
+  const [category, setCategory] = useState('Africa');
   const [isPublished, setIsPublished] = useState(false);
 
   const generateContent = async ()=>{
@@ -18,7 +23,33 @@ const AddBlog = () => {
   }
 
   const onSubmitHandler = async (e) => {
-    e.preventDefault();
+    try {
+      e.preventDefault();
+      setIsAdding(true)
+
+      const blog = {title, subTitle, description: quillRef.current.root.innerHTML, category, isPublished}
+
+      const formData = new FormData();
+      formData.append('blog', JSON.stringify(blog))
+      formData.append('image', image)
+
+      const {data} = await axios.post('/api/blog/add', formData);
+
+      if(data.success){
+        toast.success(data.message);
+        setImage(false)
+        setTitle('')
+        quillRef.current.root.innerHTML = ''
+        setCategory('Africa')
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }finally{
+      setIsAdding(false)
+    }
+    
   }
 
   useEffect(()=>{
@@ -88,7 +119,7 @@ const AddBlog = () => {
         <p>Publish Now</p>
         <input type='checkbox' checked={isPublished} className='scale-125 cursor-pointer' onChange={e => setIsPublished(e.target.checked)}></input>
       </div>
-      <button type='submit'className='mt-8 w-40 h-10 bg-accent text-white rounded cursor-pointer text-sm'>Add Blog</button>
+      <button disabled={isAdding} type='submit'className='mt-8 w-40 h-10 bg-accent text-white rounded cursor-pointer text-sm'>{isAdding ? 'Adding...' : 'Add Blog'}</button>
 
       </div>
     </form>
