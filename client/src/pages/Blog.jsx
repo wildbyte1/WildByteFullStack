35053144f864b1f31c2878f824580a85c79cddd6
Angelica Victoria <angelica@avictoria.dev>
2025-07-29 21:07:18 -0700

    updated Blog.jsx to:
    connect backend apis with frontend blog app

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -5,30 +5,56 @@ import Navbar from '../components/Navbar';
 import Moment from 'moment';
 import Footer from '../components/Footer.jsx';
 import Loader from '../components/Loader.jsx';
+import { useAppContext } from '../context/AppContext.jsx';
+import toast from 'react-hot-toast';
 
 const Blog = () => {
 
   const {id} = useParams()
 
+  const {axios} = useAppContext()
+
   const [data, setData] = useState(null)
   const [comments, setComments] = useState([]);
-
   const [name, setName] = useState('');
   const [content, setContent] = useState('');
 
   const fetchBlogData = async () => {
-    const data = blog_data.find(item => item._id === id)
-    setData(data) 
-
+    try {
+      const {data} = await axios.get(`/api/blog/${id}`)
+      data.success ? setData(data.blog) : toast.error(data.message)
+    } catch (error) {
+      toast.error(data.message);
+    }
   }
+    
 
   const fetchComments = async () => {
-    setComments(comments_data)
-  }
+    try {
+      const {data} = await axios.post('/api/blog/comments', {blogId: id})
+      if (data.success){
+        setComments(data.comments)
+      }else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+  }}
 
   const addComment = async (e) => {
     e.preventDefault();
-    
+    try {
+      const { data } = await axios.post('/api/blog/add-comment', { blog: id, name, content });
+      if (data.success) {
+        toast.success(data.message)
+        setName('')
+        setContent('')
+    }else{
+        toast.error(data.message)
+    } 
+  }catch (error) {
+      toast.error(error.message)
+    }
   }
 
   useEffect(()=>{
