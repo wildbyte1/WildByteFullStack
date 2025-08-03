1deb7614cd2abf6cef495720409ebd64f58861f8
Angelica Victoria <angelica@avictoria.dev>
2025-08-03 16:01:49 -0700

    updated BlogTableItem.jsx
    connected backend Apis with frontend blogg app

--- client/src/components/admin/BlogTableItem.jsx
+++ client/src/components/admin/BlogTableItem.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import { assets } from '../../assets/assets.js';
+import toast from 'react-hot-toast';
+import { useAppContext } from '../../context/AppContext.jsx';
 
 const BlogTableItem = ({blog, fetchBlogs, index }) => {
 
     const {title, createdAt} = blog;
     const BlogDate = new Date(createdAt)
+
+    const { axios } = useAppContext();
+
+    const deleteBlog = async () => {
+      const confirm = window.confirm('Are you sure you want to delete this blog?')
+      if(!confirm) return;
+      try {
+        const {data} = await axios.post('/api/blog/delete', {id: blog._id})
+        if(data.success) {
+          toast.success(data.message)
+          await fetchBlogs()
+        }else{
+          toast.error(data.message)
+        }
+      } catch (error) {
+        toast.error(error.message);
+      }
+    }
+
+    const togglePublish = async () => {
+      try {
+        const { data } = await axios.post('/api/blog/toggle-publish', {id: blog._id})
+        if (data.success) {
+          toast.success(data.message);
+          await fetchBlogs();
+        } else {
+          toast.error(data.message);
+        } 
+      } catch (error) {
+        toast.error(error.message)
+      }
+    }
 
   return (
     <tr className='border-y border-gray-300'>
@@ -21,8 +55,8 @@ const BlogTableItem = ({blog, fetchBlogs, index }) => {
         </p>
       </td>
       <td className='px-2 py-4 flex text-xs gap-3'>
-        <button className='border px-2 py-0.5 mt-1 rounded cursor-pointer'>{blog.isPublished ? 'Unpublish' : 'Publish'}</button>
-        <img src={assets.cross_icon} className='w-8 hover:scale-110 transition-all cursor-pointer' alt='delete' />
+        <button onClick={togglePublish} className='border px-2 py-0.5 mt-1 rounded cursor-pointer'>{blog.isPublished ? 'Unpublish' : 'Publish'}</button>
+        <img src={assets.cross_icon} className='w-8 hover:scale-110 transition-all cursor-pointer' alt='delete' onClick={deleteBlog}/>
       </td>
     </tr>
   );
