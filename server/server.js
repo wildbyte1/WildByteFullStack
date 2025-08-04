9c546bee6cd8321a9ec8f49a4343bd1400de564a
Angelica Victoria <angelica@avictoria.dev>
2025-08-04 16:52:12 -0700

    fixed backend vercel.json

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-import express from 'express'
-import 'dotenv/config'
-import cors from 'cors'
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
 import connectDB from './configs/db.js';
 import adminRouter from './routes/adminRoutes.js';
 import blogRouter from './routes/blogRoutes.js';
 
 const app = express();
 
-await connectDB()
+await connectDB();
 
 // Middlewares
 app.use(
@@ -19,14 +19,11 @@ app.use(
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res)=> res.send("API is Working"))
-app.use('/api/admin', adminRouter)
-app.use('/api/blog', blogRouter)
- 
-const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('API is Working'));
+app.use('/api/admin', adminRouter);
+app.use('/api/blog', blogRouter);
 
-app.listen(PORT, ()=> {
-    console.log('Server is running on port' + PORT)
-})
+// ✅ DO NOT use app.listen on Vercel
+// ❌ app.listen(PORT, () => console.log(...))
 
 export default app;
