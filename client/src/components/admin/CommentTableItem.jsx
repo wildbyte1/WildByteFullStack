32ea3dba29380c78bad2bc2ade55ee9335f0d6b6
Angelica Victoria <angelica@avictoria.dev>
2025-08-03 17:34:06 -0700

    updated CommentTableItem.jsx and Comments.jsx
    connected backend APIs to frontend blog app.

--- client/src/components/admin/CommentTableItem.jsx
+++ client/src/components/admin/CommentTableItem.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import { assets } from '../../assets/assets';
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 const CommentTableItem = ({comment, fetchComments}) => {
 
     const { blog, createdAt, _id } = comment;
     const BlogDate = new Date(createdAt);
+
+    const { axios } = useAppContext()
+
+    const approveComment = async () =>{
+      try {
+        const {data} = await axios.post('/api/admin/approve-comment', {id: _id})
+        if (data.success) {
+          toast.success(data.message)
+          fetchComments()
+        }else{
+          toast.error(data.message)
+        }
+      } catch (error) {
+        toast.error(error.message)
+      }
+    }
+
+    const deleteComment = async () =>{
+      try {
+        const confirm = window.confirm('Are you sure you want to delete this comment?');
+        if(!confirm) return;
+
+        const {data} = await axios.post('/api/admin/delete-comment', {id: _id})
+        if (data.success) {
+          toast.success(data.message)
+          fetchComments()
+        }else{
+          toast.error(data.message)
+        }
+      } catch (error) {
+        toast.error(error.message)
+      }
+    }
 
   return (
     <tr className='border-y border-gray-300'>
@@ -15,7 +50,7 @@ const CommentTableItem = ({comment, fetchComments}) => {
         <b className='font-medium text-gray-600'>Name</b> : {comment.name}
         <br />
         <br/>
-        <b className='font-medium text-gray-600'>Comment</b> : {comment.comment}
+        <b className='font-medium text-gray-600'>Comment</b> : {comment.content}
         </td>
         <td className='px-6 py-4 max-sm:hidden'>
             {BlogDate.toLocaleDateString()}
--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { comments_data } from '../../assets/assets'
 import CommentTableItem from '../../components/admin/CommentTableItem'
+import { useAppContext } from '../../context/AppContext'
+import toast from 'react-hot-toast'
 
 const Comments = () => {
 
-  const [comments, setComments] = useState([])  
-  const [filter, setFilter] = useState('Not Approved')
+  const [comments, setComments] = useState([]);
+  const [filter, setFilter] = useState('Not Approved');
+
+  const { axios } = useAppContext();
 
   const fetchComments = async () => {
-    setComments(comments_data)
+    try {
+      const { data } = await axios.get('/api/admin/comments');
+      data.success ? setComments(data.comments) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
   }
 
   useEffect(() => {
-    fetchComments()
+    fetchComments();
   }, [])
 
   return (
@@ -20,7 +29,8 @@ const Comments = () => {
       <div className='flex justify-between items-center max-w-3xl'>
         <h1>Comments</h1>
         <div className='flex gap-4'>
-          <button onClick={() => setFilter('Approved')}
+          <button
+            onClick={() => setFilter('Approved')}
             className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${
               filter === 'Approved' ? 'text-text' : 'text-alternative'
             }`}
@@ -28,35 +38,54 @@ const Comments = () => {
             Approved
           </button>
 
-          <button onClick={() => setFilter('Not Approved')}
+          <button
+            onClick={() => setFilter('Not Approved')}
             className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${
               filter === 'Not Approved' ? 'text-text' : 'text-alternative'
             }`}
           >
+            {' '}
             Not Approved
           </button>
         </div>
       </div>
       <div className='relative h-4/5 max-w-3xl overflow-x-auto mt-4 bg-white shadow rounded-lg scrollbar-hide'>
         <table className='w-full text-sm text-gray-500'>
-            <thead className='text-xs text-gray-700 text-left uppercase'>
+          <thead className='text-xs text-gray-700 text-left uppercase'>
             <tr>
-              <th scope='col' className='px-6 py-3'>Blog Title & Comment </th>
-              <th scope='col' className='px-6 py-3 max-sm:hidden'>Date</th>
-              <th scope='col' className='px-6 py-3'>Actions</th>
+              <th scope='col' className='px-6 py-3'>
+                {' '}
+                Blog Title & Comment{' '}
+              </th>
+              <th scope='col' className='px-6 py-3 max-sm:hidden'>
+                {' '}
+                Date{' '}
+              </th>
+              <th scope='col' className='px-6 py-3'>
+                {' '}
+                Action{' '}
+              </th>
             </tr>
-            </thead>
-            <tbody>
-              {comments.filter((comment) => { 
-                if(filter === 'Approved') 
-                  return comment.isApproved === true;
+          </thead>
+          <tbody>
+            {comments
+              .filter((comment) => {
+                if (filter === 'Approved') return comment.isApproved === true;
                 return comment.isApproved === false;
-}).map((comment, index)=> <CommentTableItem  key={comment._id} comment={comment} index={index + 1} fetchComments={fetchComments} />)}
-            </tbody>
+              })
+              .map((comment, index) => 
+                <CommentTableItem
+                  key={comment._id}
+                  comment={comment}
+                  index={index + 1}
+                  fetchComments={fetchComments}
+                />
+              )}
+          </tbody>
         </table>
       </div>
     </div>
   );
-}
+};
 
 export default Comments
