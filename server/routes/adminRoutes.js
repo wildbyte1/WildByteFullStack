2bf6a14ee308783e0eda60cbb70cf17017aea5fc
Angelica Victoria <angelica@avictoria.dev>
2025-08-08 14:14:19 -0700

    updated adminRoutes.js

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,15 +1,24 @@
-import express from "express";
-import { adminLogin, approveCommentById, deleteCommentById, getAllBlogsAdmin, getAllComments, getDashboard } from "../controllers/adminController.js";
-import auth from "../middleware/auth.js";
+import express from 'express';
+import {
+  adminLogin,
+  approveCommentById,
+  deleteCommentById,
+  getAllBlogsAdmin,
+  getAllComments,
+  getDashboard,
+} from '../controllers/adminController.js';
+import auth from '../middleware/auth.js';
 
 const adminRouter = express.Router();
 
-adminRouter.post("/login", adminLogin);
-adminRouter.get("/comments", auth, getAllComments);
-adminRouter.get('/blogs', auth, getAllBlogsAdmin);
-adminRouter.post('/delete-comment', auth, deleteCommentById);
-adminRouter.post('/approve-comment', auth, approveCommentById);
-adminRouter.get('/dashboard', auth, getDashboard);
+// Public
+adminRouter.post('/login', adminLogin);
 
+// Auth-protected routes
+adminRouter.get('/dashboard', auth, getDashboard);
+adminRouter.get('/blogs', auth, getAllBlogsAdmin);
+adminRouter.get('/comments', auth, getAllComments);
+adminRouter.post('/approve-comment', auth, approveCommentById);
+adminRouter.post('/delete-comment', auth, deleteCommentById);
 
 export default adminRouter;
